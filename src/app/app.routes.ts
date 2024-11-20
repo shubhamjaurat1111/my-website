862ba3d426c5c65180a9126e2938f0ac130df108
shubhamjaurat1111 <shubhamjaurat1111@gmail.com>
2024-11-20 12:52:41 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'portfolio',
+        loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule)
+    }
+];
